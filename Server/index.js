f39913a664f936f69c6bae7d910e7d0bac2e5ef0
fatemehparchiani <50096820+fatemehparchiani@users.noreply.github.com>
2019-05-15 18:16:03 +0430

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -1,13 +1,20 @@
 
+
 var express = require('express');
 var app = express();
 var path = require('path');
 app.use(express.static('assets'))
 
+var list_messages = {
+    msg: [ ],
+    name: [ ]
+};
+
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/../index.html'));
 });
+
 
 app.listen(8080);
 
@@ -19,19 +26,40 @@ var wss = new server({ port: 5001 });
 wss.on('connection', function (ws) {
 
     //on message event 
-    ws.on('message', (data) => {
+    console.log(list_messages.msg.length);
+    for (i = 0; i < list_messages.msg.length; i++) {
+        var mesage = list_messages.msg[i];
+
+        var data = JSON.stringify({
+            name: list_messages.name[i],
+            message: list_messages.msg[i]
+        });
+
         console.log(data);
-        // var jsonObject = JSON.parse(data);
-        // var username = jsonObject.name;
-        // var message = jsonObject.message;
+        ws.send(data);
+    }
+    
+    
+    ws.on('message', (data) => {
+        //console.log(data);
+        var jsonObject = JSON.parse(data);
+        console.log(data);
+         var username = jsonObject.name;
+         var message = jsonObject.message;
+         con.log("username");
+         list_messages.msg[list_messages.msg.length]= message;
+         list_messages.name[list_messages.name.length] = username;
+         console.log(list_messages);
 
         //broadcast data to all clients 
         wss.clients.forEach(function e(client) {
             client.send(data);
         })
     });
-
+    
     ws.on('close', function () {
         console.log('Connection terminated..Closing Client');
+     
     });
 })
+console.log("ok!");
